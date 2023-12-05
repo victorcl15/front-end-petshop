c15ede2558fa78a317cab0869cffd7fa9f65c8d4
victor.calle <victor.calle@est.iudigital.edu.co>
2023-12-04 22:23:34 -0500

    correcion para el contexto global

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,10 +1,11 @@
-
-import React from "react";
-import { createContext, useContext, useReducer, useMemo, useState } from "react";
-
-
-
-
+import React, { useEffect } from "react";
+import {
+  createContext,
+  useContext,
+  useReducer,
+  useMemo,
+  useState,
+} from "react";
 
 //--------->
 
@@ -15,14 +16,25 @@ export const useProducto = () => {
 };
 
 export const ProductoProvider = ({ children }) => {
-
-  const initialProducto = useMemo(() => localStorage.getItem("producto") || "", []);
+  const initialProducto = useMemo(() => {
+    const storedProducto = localStorage.getItem("producto");
+    return storedProducto ? JSON.parse(storedProducto) : "";
+  }, []);
+  console.log(initialProducto);
   //const [token, setToken] = useState(initialToken);
   const [producto, setProducto] = useState(initialProducto);
 
+  useEffect(() => {
+    const storedProducto = localStorage.getItem("producto");
+    if (storedProducto) {
+      setProducto(JSON.parse(storedProducto));
+      console.log(JSON.parse(storedProducto));
+    }
+  }, []); // Se ejecutará solo una vez al montar el component
+
   const productoElegido = (newProducto) => {
     setProducto(newProducto);
-    localStorage.setItem("producto", newProducto);
+    localStorage.setItem("producto", JSON.stringify(newProducto));
   };
 
   const limpiarProducto = () => {
@@ -30,7 +42,13 @@ export const ProductoProvider = ({ children }) => {
     localStorage.removeItem("producto");
   };
 
-  return <ProductoContext.Provider value={{ producto, productoElegido, limpiarProducto }}>{children}</ProductoContext.Provider>;
+  return (
+    <ProductoContext.Provider
+      value={{ producto, productoElegido, limpiarProducto }}
+    >
+      {children}
+    </ProductoContext.Provider>
+  );
 };
 
 //-----
@@ -43,14 +61,25 @@ export const useLogin = () => {
 };
 
 export const LoginProvider = ({ children }) => {
-  const initialLogin = useMemo(() => localStorage.getItem("usuario") || "", []);
+  const initialLogin = useMemo(() => {
+    const storedUsuario = localStorage.getItem("usuario");
+    return storedUsuario ? JSON.parse(storedUsuario) : "";
+  }, []);
   //const [token, setToken] = useState(initialToken);
   const [usuario, setUsuario] = useState(initialLogin);
 
+  useEffect(() => {
+    const storedUsuario = localStorage.getItem("usuario");
+    if (storedUsuario) {
+      setUsuario(JSON.parse(storedUsuario));
+      console.log(JSON.parse(storedUsuario));
+    }
+  }, []); // Se ejecutará solo una vez al montar el componente
+
   const login = (newUsuario) => {
-    console.log(newUsuario)
+    console.log(newUsuario);
     setUsuario(newUsuario);
-    localStorage.setItem("usuario", newUsuario);
+    localStorage.setItem("usuario", JSON.stringify(newUsuario));
   };
 
   const logout = () => {
@@ -58,9 +87,11 @@ export const LoginProvider = ({ children }) => {
     localStorage.removeItem("usuario");
   };
 
-  return <LoginContext.Provider value={{ usuario, login, logout }}>{children}</LoginContext.Provider>;
+  return (
+    <LoginContext.Provider value={{ usuario, login, logout }}>
+      {children}
+    </LoginContext.Provider>
+  );
 };
 
 //-----
-
-
