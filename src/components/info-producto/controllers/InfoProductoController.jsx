b924008bb43309cdb0acd75099c5f2cb68e46f2c
victor.calle <victor.calle@est.iudigital.edu.co>
2023-12-05 19:00:42 -0500

    cambios nuevos

--- src/components/info-producto/controllers/InfoProductoController.jsx
+++ src/components/info-producto/controllers/InfoProductoController.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { InfoProducto } from "../modules/InfoProducto";
 import { useProducto } from "../../../context";
+import { getProductoPorId } from "../services/infoProductoService";
 
 export function InfoProductoController() {
     const [expanded, setExpanded] = useState(false);
+    const [infoProducto, setInfoProducto] = useState();
     const { producto, productoElegido, limpiarProducto } = useProducto();
     const handleExpandClick = () => {
       setExpanded(!expanded);
     };
+
+    useEffect(() => {
+      if(producto.id !== "" && producto.id){
+       const result = getProductoPorId(producto.id);
+       setInfoProducto(result);
+      }
+      
+    }, [])
   return <>
   <InfoProducto
   expanded={expanded}
   handleExpandClick={handleExpandClick}
   producto={producto}
+  infoProducto={infoProducto}
   ></InfoProducto>
   </>;
 }
--- src/components/info-producto/index.jsx
+++ src/components/info-producto/index.jsx
@@ -1,8 +1,10 @@
+import NavBarIndex from "../navbar";
 import { InfoProductoController } from "./controllers/InfoProductoController";
 
 export default function InfoProductoIndex() {
   return (
     <>
+      <NavBarIndex></NavBarIndex>
       <InfoProductoController></InfoProductoController>
     </>
   );
